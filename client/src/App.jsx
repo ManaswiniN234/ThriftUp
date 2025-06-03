dcbe6c4afdef001ba17873e90ade7be5374c279b
maverickOG <heysajit@gmail.com>
2025-06-03 22:27:01 +0530

    Tailwind issue resolved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css'
+import React from 'react';
+// Remove App.css import since it's causing issues
+// import './App.css';
+import Navbar from './Components/Layout/Navbar.jsx';
 
 function App() {
-
   return (
-    <>
-    </>
+      <Navbar />
   )
 }
 
-export default App
+export default App;
